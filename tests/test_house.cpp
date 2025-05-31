2a9aff8aa75aaffc5c29afd55ffb72689119bb04
CoF-Luke <vladpilot.new@gmail.com>
2025-06-01 02:35:31 +0300

    yet another test

--- tests/test_house.cpp
+++ tests/test_house.cpp
@@ -1,4 +1,21 @@
-#include <city.h>
+/* #include <house.h>
 #include <xmlvisitor.h>
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+
+using ::testing::_;
+
+class MockxmlVisitor : public xmlVisitor
+{
+public:
+    MOCK_METHOD(void, industryVisitor, (Industry* industry), (override));
+};
+
+TEST(IndustryTests, Accept)
+{
+    MockxmlVisitor visitor;
+    Industry i1("cast", 100, 1, 3);
+    EXPECT_CALL(visitor, industryVisitor(_)).Times(1);
+    i1.accept(&visitor);
+}
+ */
