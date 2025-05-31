53e181924852330e9b2d7a629f826e870a684a00
CoF-Luke <vladpilot.new@gmail.com>
2025-05-31 23:57:54 +0300

    add tests

--- tests/test_city.cpp
+++ tests/test_city.cpp
@@ -1,17 +1,17 @@
 #include <city.h>
 #include <xmlvisitor.h>
-//#include <gmock/gmock.h>
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 class MockxmlVisitor : public xmlVisitor
 {
 public:
-    MOCK_METHOD(void, cityVisitor, (City*), (override));
+    MOCK_METHOD(void, cityVisitor, (City* city), (override));
 };
 
 TEST(City, Accept)
 {
-    MockXmlVisitor visitor;
+    MockxmlVisitor visitor;
     City c1("tehran", 1, 2, 100);
     EXPECT_CALL(visitor, cityVisitor()).Times(1);
     c1.accept(&visitor);
