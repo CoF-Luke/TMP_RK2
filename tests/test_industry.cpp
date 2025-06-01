c11f6f016d543caa7ae5fd4feb19bc328af34440
CoF-Luke <vladpilot.new@gmail.com>
2025-06-01 03:27:11 +0300

    yet another test

--- tests/test_industry.cpp
+++ tests/test_industry.cpp
@@ -11,8 +11,14 @@ class MockxmlVisitor : public xmlVisitor
 {
 public:
     MOCK_METHOD(void, cityVisitor, (City* city), (override));
-    MOCK_METHOD(void, industryVisitor, (Industry* industry), (override));
     MOCK_METHOD(void, houseVisitor, (House* house), (override));
+    MOCK_METHOD(void, industryVisitor, (Industry* industry), (override));
+};
+
+class MockHouse : public House
+{
+public:
+    MOCK_METHOD(int, getStairs(), (City* city), (override));
 };
 
 TEST(CityTest, Accept)
@@ -23,6 +29,28 @@ TEST(CityTest, Accept)
     c1.accept(&visitor);
 }
 
+TEST(HouseTest, Accept)
+{
+    MockxmlVisitor visitor;
+    MockHouse h1(3, 2, 3);
+    EXPECT_CALL(visitor, houseVisitor(_)).Times(1);
+    EXPECT_CALL(h1, getStairs()).Times(1);
+    h1.accept(&visitor);
+}
+
+TEST(HouseTest, Accept)
+{
+    House h1(3, 2, 3);
+    EXPECT_EQ(h1.getStairs(), 3);
+}
+
+/* TEST(HouseTest, Accept)
+{
+    MockHouse h1(3, 2, 3);
+    EXPECT_CALL(h1, getStairs()).Times(1);
+    h1.accept(&visitor);
+} */
+
 TEST(IndustryTests, Accept)
 {
     MockxmlVisitor visitor;
@@ -30,13 +58,4 @@ TEST(IndustryTests, Accept)
     EXPECT_CALL(visitor, industryVisitor(_)).Times(1);
     i1.accept(&visitor);
 }
-
-TEST(HouseTest, Accept)
-{
-    MockxmlVisitor visitor;
-    House h1(3, 2, 3);
-    EXPECT_CALL(visitor, houseVisitor(_)).Times(1);
-    h1.accept(&visitor);
-}
-
 
