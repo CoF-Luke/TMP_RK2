11a92de7e909b93485bb5e0b899325198b1a97bf
CoF-Luke <vladpilot.new@gmail.com>
2025-06-01 04:13:18 +0300

    yet another test

--- src/xmlvisitor.cpp
+++ src/xmlvisitor.cpp
@@ -1,37 +1,22 @@
-#include <city.h>
-#include <house.h>
-#include <industry.h>
-#include <visitor.h>
-#include <xmlvisitor.h>
+#include "xmlvisitor.h"
+#include "house.h"
 
-#include <vector>
-#include <iostream>
-#include <sstream>
-#include <gtest/gtest.h>
+xmlVisitor::xmlVisitor()
+{
 
-TEST(MainTest, OutputToConsole) {
-    std::streambuf* oldCout = std::cout.rdbuf();
-    std::stringstream capture;
-    std::cout.rdbuf(capture.rdbuf());
+}
 
-    std::vector<Place*> mapItems;
-    City c1("tehran", 1, 2, 100);
-    mapItems.push_back(&c1);
-    House h1(3, 2, 3);
-    mapItems.push_back(&h1);
-    Industry i1("cast", 100, 1, 3);
-    mapItems.push_back(&i1);
-    xmlVisitor xml;
+void xmlVisitor::cityVisitor(City *city)
+{
 
-    for(auto it = mapItems.begin(); it != mapItems.end(); it++)
-    {
-        (*it)->accept(&xml);
-    }
+}
 
-    std::cout.rdbuf(oldCout);
+void xmlVisitor::houseVisitor(House *house)
+{
+    std::cout << house->getStairs() << std::endl;
+}
 
-    std::string output = capture.str();
-    EXPECT_EQ(output.find("Hello, world!"), "3");
+void xmlVisitor::industryVisitor(Industry *industry)
+{
 
-    cout << "Expected: 3 \nOutput: " << output << endl; 
 }
--- tests/test_xmlvisitor.cpp
+++ tests/test_xmlvisitor.cpp
@@ -1,4 +1,37 @@
-/* #include <city.h>
+#include <city.h>
+#include <house.h>
+#include <industry.h>
+#include <visitor.h>
 #include <xmlvisitor.h>
-#include <gmock/gmock.h>
-#include <gtest/gtest.h> */
+
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <gtest/gtest.h>
+
+TEST(MainTest, OutputToConsole) {
+    std::streambuf* oldCout = std::cout.rdbuf();
+    std::stringstream capture;
+    std::cout.rdbuf(capture.rdbuf());
+
+    std::vector<Place*> mapItems;
+    City c1("tehran", 1, 2, 100);
+    mapItems.push_back(&c1);
+    House h1(3, 2, 3);
+    mapItems.push_back(&h1);
+    Industry i1("cast", 100, 1, 3);
+    mapItems.push_back(&i1);
+    xmlVisitor xml;
+
+    for(auto it = mapItems.begin(); it != mapItems.end(); it++)
+    {
+        (*it)->accept(&xml);
+    }
+
+    std::cout.rdbuf(oldCout);
+
+    std::string output = capture.str();
+    EXPECT_EQ(output.find("Hello, world!"), "3");
+
+    cout << "Expected: 3 \nOutput: " << output << endl; 
+}
